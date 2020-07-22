f853c1a350f5ef709db218539d81bc52040e77f1
kip <cyruskiprop254@gmail.com>
2020-07-22 22:36:29 +0300

    add all the five scene to the game entry point

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ import CreditsScene from './Scenes/CreditsScene';
 class Game extends Phaser.Game {
     constructor() {
         super(config);
+        this.scene.add('Boot', BootScene);
+        this.scene.add('Preloader', PreloaderScene);
+        this.scene.add('Title', TitleScene);
+        this.scene.add('Options', OptionsScene);
+        this.scene.add('Credits', CreditsScene);
         this.scene.add("Game", GameScene);
         this.scene.start("Game");
     }
