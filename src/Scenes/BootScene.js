24567e4717ed71d6ed1ba51b85b60cfcff84c9a7
kip <cyruskiprop254@gmail.com>
2020-07-22 22:43:00 +0300

    :sparkles: Boot
    add boot scene to the game

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -1,11 +1,17 @@
 import 'phaser';
+import
 
 export default class BootScene extends Phaser.Scene {
     constructor() {
         super('Boot');
     }
 
-    preload() {}
+    preload() {
+        this.load.image('logo', 'assets/logo.png')
+    }
 
-    create() {}
+    create() {
+        // execute the preloader a soon as the boot scene is complete
+        this.scene.start('Preloader')
+    }
 };
