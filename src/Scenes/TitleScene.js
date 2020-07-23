e95c786473c0fceac1352dcf970fe25d493e180a
kip <cyruskiprop254@gmail.com>
2020-07-23 11:44:09 +0300

    :lipstick: Title scene
    add play button, and event handlers for the input objects

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -1,11 +1,31 @@
 import 'phaser';
 
 export default class TitleScene extends Phaser.Scene {
-    constructor() {
-        super('Title');
-    }
+        constructor() {
+            super('Title');
+        }
 
-    preload() {}
+        preload() {}
 
-    create() {}
-};
+        create() {
+            this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+            this.centerButton(this.gameButton, 1);
+
+            this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
+            this.centerButtonText(this.gameText, this.gameButton);
+
+            this.gameButton.on('pointerdown', function(pointer) {
+                this.scene.start('Game');
+            }.bind(this));
+
+            this.input.on('pointerover', function(event, gameObjects) {
+                gameObjects[0].setTexture('blueButton2');
+            });
+
+            this.input.on('pointerout', function(event, gameObjects) {
+                gameObjects[0].setTexture('blueButton1');
+            });
+
+
+
+        };
