e8c01ef7b8ad4149281c961a5f0b97dbf8c7f522
kip <cyruskiprop254@gmail.com>
2020-07-24 01:31:27 +0300

    :lipstick: Assets files
    add more images and music files

--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -8,8 +8,7 @@ export default class OptionsScene extends Phaser.Scene {
   preload() {}
 
   create() {
-    this.musicOn = true;
-    this.soundOn = true;
+    this.model = this.sys.game.globals.model;
 
     this.text = this.add.text(300, 100, "Options", { fontSize: 40 });
     this.musicButton = this.add.image(200, 200, "checkedBox");
@@ -24,7 +23,7 @@ export default class OptionsScene extends Phaser.Scene {
     this.musicButton.on(
       "pointerdown",
       function () {
-        this.musicOn = !this.musicOn;
+        this.model.musicOn = !this.model.musicOn;
         this.updateAudio();
       }.bind(this)
     );
@@ -32,7 +31,7 @@ export default class OptionsScene extends Phaser.Scene {
     this.soundButton.on(
       "pointerdown",
       function () {
-        this.soundOn = !this.soundOn;
+        this.model.soundOn = !this.model.soundOn;
         this.updateAudio();
       }.bind(this)
     );
@@ -53,13 +52,13 @@ export default class OptionsScene extends Phaser.Scene {
     );
   }
   updateAudio() {
-    if (this.musicOn === false) {
+    if (this.model.musicOn === false) {
       this.musicButton.setTexture("box");
     } else {
       this.musicButton.setTexture("checkedBox");
     }
 
-    if (this.soundOn === false) {
+    if (this.model.soundOn === false) {
       this.soundButton.setTexture("box");
     } else {
       this.soundButton.setTexture("checkedBox");
--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import TitleScene from "./Scenes/TitleScene";
 import OptionsScene from "./Scenes/OptionsScene";
 import CreditsScene from "./Scenes/CreditsScene";
 
+import Model from "./Model";
+
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    const model = new Model();
+    this.globals = { model };
     this.scene.add("Boot", BootScene);
     this.scene.add("Preloader", PreloaderScene);
     this.scene.add("Title", TitleScene);
