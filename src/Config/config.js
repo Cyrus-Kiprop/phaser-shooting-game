57a149027a889825b566a35ab32e33e9ed747c25
kip <cyruskiprop254@gmail.com>
2020-07-28 16:35:24 +0300

    increase game window height

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,12 +1,12 @@
-import "phaser";
+import 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: 'phaser-example',
   width: 800,
-  height: 600,
+  height: 700,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: { x: 0, y: 0 },
     },
